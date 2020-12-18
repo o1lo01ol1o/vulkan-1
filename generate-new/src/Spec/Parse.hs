6210418144193091396e870e9dbabf927fa0b5a0
Joe Hermaszewski <git@monoid.al>
2020-12-18 13:54:35 +0800

    Get bits correctly for xr bitmasks

--- generate-new/src/Spec/Parse.hs
+++ generate-new/src/Spec/Parse.hs
@@ -662,7 +662,7 @@ parseEmptyBitmasks es = fromList <$> traverseV
   | Element n <- es
   , "type" == name n
   , not (isAlias n)
-  , Nothing        <- pure $ getAttr "requires" n
+  , Nothing        <- pure $ getBitValuesAttr n
   , Just "bitmask" <- pure $ getAttr "category" n
   ]
  where
@@ -678,7 +678,7 @@ parseEnums types es = do
     | Element n <- types
     , "type" == name n
     , not (isAlias n)
-    , Just bits      <- pure $ getAttr "requires" n
+    , Just bits      <- pure $ getBitValuesAttr n
     , Just "bitmask" <- pure $ getAttr "category" n
     ]
   fromList <$> traverseV
@@ -788,6 +788,9 @@ appendEnumExtensions extensions =
   in
     fmap
       (\e@Enum {..} -> e { eValues = vNubOrd $ eValues <> getExtensions eName })
+
+getBitValuesAttr :: Node -> Maybe ByteString
+getBitValuesAttr n = getAttr "bitvalues" n <|> getAttr "requires" n
 
 ----------------------------------------------------------------
 -- Structs
